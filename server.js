4f05565363c01dc2c595923c65f2161e57cad8df
Dheeraj <dheeraj.s.roy@gmil.com>
2023-04-30 13:44:59 +0530

    ProductController

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 // 1. Import Exprerss
 import express from 'express';
-import * as ProductRouter from './src/features/product/product.routes';
-
+import productRouter from './src/features/product/product.routes.js';
 // 2. Create Server
 const server = express();
 
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/products
-server.use('/api/products', ProductRouter);
+server.use('/api/products', productRouter);
 
 // 3. Default request handler
 server.get('/', (req, res) => {
