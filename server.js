ab33db57c5b2036ada919b9ff5db94c837e0038b
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-07-16 11:13:42 +0530

    post request

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // 1. Import Exprerss
 import express from 'express';
+import bodyParser from 'body-parser';
 import productRouter from './src/features/product/product.routes.js';
 // 2. Create Server
 const server = express();
 
+server.use(bodyParser.json())
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/products
 server.use('/api/products', productRouter);
