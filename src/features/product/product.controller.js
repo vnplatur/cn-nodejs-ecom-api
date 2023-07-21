3a4113f6553224c296b04fa06a5c0f3033d25bf0
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-07-21 16:03:44 +0530

    Use next to pass error

--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -19,11 +19,12 @@ export default class ProductController {
     res.status(201).send(createdRecord);
   }
 
-  rateProduct(req, res) {
+  rateProduct(req, res, next) {
     console.log(req.query);
+    try{
     const userID = req.query.userID;
     const productID = req.query.productID;
-    const rating = req.query.rating;
+    const rating = req.querys.rating;
     ProductModel.rateProduct(
       userID,
       productID,
@@ -32,7 +33,12 @@ export default class ProductController {
     return res
       .status(200)
       .send('Rating has been added');
-  }
+  
+    }catch(err){
+      console.log("Passing error to middleware");
+      next(err);
+    }
+    }
 
   getOneProduct(req, res) {
     const id = req.params.id;
