d71ae01cb8b349ab27590bc061c7839f4a4e906a
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-07-21 12:30:52 +0530

    winston_logger

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,18 +1,28 @@
 import fs from 'fs';
+import winston from "winston";
 
 const fsPromise = fs.promises;
 
-async function log(logData) {
-  try {
-    logData = `\n ${new Date().toString()} - ${logData}`;
-    await fsPromise.appendFile(
-      'log.txt',
-      logData
-    );
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function log(logData) {
+//   try {
+//     logData = `\n ${new Date().toString()} - ${logData}`;
+//     await fsPromise.appendFile(
+//       'log.txt',
+//       logData
+//     );
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+const logger = winston.createLogger({
+  level:'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'request-logging' },
+  transports:[
+    new winston.transports.File({filename:'logs.txt'})
+  ]
+});
 
 const loggerMiddleware = async (
   req,
@@ -24,7 +34,7 @@ const loggerMiddleware = async (
     const logData = `${
       req.url
     } - ${JSON.stringify(req.body)}`;
-    await log(logData);
+    logger.info(logData)
   }
   next();
 };
