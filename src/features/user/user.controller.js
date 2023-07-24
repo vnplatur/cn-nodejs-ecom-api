0d2fa10885060ed260a86968a04143f58d71a48f
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-07-24 12:44:52 +0530

    Use bcrypt

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,6 +1,7 @@
 import UserModel from './user.model.js';
 import jwt from 'jsonwebtoken';
 import UserRepository from './user.repository.js';
+import bcrypt from 'bcrypt';
 
 export default class UserController {
 
@@ -15,10 +16,12 @@ export default class UserController {
       password,
       type,
     } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 12)
     const user = new UserModel(
       name,
       email,
-      password,
+      hashedPassword,
       type
     );
     await this.userRepository.signUp(user);
@@ -27,28 +30,34 @@ export default class UserController {
 
   async signIn(req, res, next) {
     try{
-    const result = await this.userRepository.signIn(
-      req.body.email,
-      req.body.password
-    );
-    if (!result) {
+      // 1. Find user by email.
+    const user = await this.userRepository.findByEmail(req.body.email);
+    if(!user){
       return res
         .status(400)
         .send('Incorrect Credentials');
-    } else {
-      // 1. Create token.
-      const token = jwt.sign(
-        {
-          userID: result.id,
-          email: result.email,
-        },
-        'AIb6d35fvJM4O9pXqXQNla2jBCH9kuLz',
-        {
-          expiresIn: '1h',
-        }
-      );
-      // 2. Send token.
-      return res.status(200).send(token);
+    }else{
+      // 2. Compare password with hashed password.
+      const result = await bcrypt.compare(req.body.password, user.password);
+      if(result){
+ // 3. Create token.
+ const token = jwt.sign(
+  {
+    userID: result.id,
+    email: result.email,
+  },
+  'AIb6d35fvJM4O9pXqXQNla2jBCH9kuLz',
+  {
+    expiresIn: '1h',
+  }
+);
+// 4. Send token.
+return res.status(200).send(token);
+      }else{
+        return res
+        .status(400)
+        .send('Incorrect Credentials');
+      }
     }
     }catch(err){
       console.log(err);
