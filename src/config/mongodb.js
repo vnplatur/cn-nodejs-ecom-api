1e842f688d326aea7992dceba41f2e4b825360c0
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-08-13 15:58:57 +0530

    Creating_Indexes

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -14,6 +14,7 @@ export const connectToMongoDB = ()=>{
             client=clientInstance
             console.log("Mongodb is connected");
             createCounter(client.db());
+            createIndexes(client.db());
         })
         .catch(err=>{
             console.log(err);
@@ -29,4 +30,15 @@ const createCounter = async(db)=>{
     if(!existingCounter){
         await db.collection("counters").insertOne({_id:'cartItemId', value:0});
     }
+}
+
+const createIndexes = async(db)=>{
+    try{
+        await db.collection("products").createIndex({price:1});
+        await db.collection("products").createIndex({name:1, category:-1});
+        await db.collection("products").createIndex({desc:"text"});
+    }catch(err){
+        console.log(err);
+    }
+    console.log("Indexes are created");
 }
