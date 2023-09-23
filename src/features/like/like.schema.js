0f13659534689235e62bf9156c40d867ee54880f
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-09-23 12:31:07 +0530

    middlewares

--- src/features/like/like.schema.js
+++ src/features/like/like.schema.js
@@ -13,4 +13,17 @@ export const likeSchema = new mongoose.Schema({
         type:String,
         enum:['Product','Category']
     }
+}).pre('save', (next)=>{
+    console.log("New Like coming in")
+    next();
+})
+.post('save', (doc)=>{
+    console.log("Like is saved")
+    console.log(doc);
+}).pre('find', (next)=>{
+    console.log("Retriving likes");
+    next();
+}).post('find', (docs)=>{
+    console.log("Post find");
+    console.log(docs);
 })
